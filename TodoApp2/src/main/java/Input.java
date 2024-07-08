5d9d4f74d13c643f8572dd959bc6fd5ba7f4d86f
DYA <148474216+cheonghaa@users.noreply.github.com>
2024-07-08 14:31:17 +0900

    24.07.08 실습

--- TodoApp2/src/main/java/Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Input {
-    static Scanner scanner = new Scanner(System.in);
-    public static int getInt(String title) {
-        System.out.print(title);
-        return Integer.parseInt(scanner.nextLine());
-    }
-}
